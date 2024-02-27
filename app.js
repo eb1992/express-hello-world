0e749db4aed942998e0650b5362d176191757967
Erik <erik.bjornlinger@live.se>
2024-02-27 21:22:49 +0100

    added logging

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+app.get("/", (req, res) => {
+  console.log("Req: " + req + "\nRes: " + res)
+  res.type('html').send(html)});
+
+app.post("/", (req, res) => {
+  console.log("Req: " + req + "\nRes: " + res)
+});
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
